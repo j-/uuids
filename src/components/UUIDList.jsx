8816321f409d394f07465d371879043def23b049
j- <j@skeoh.com>
2016-11-17 12:54:18 +1100

    Remove unused props from UUID list component

--- src/components/UUIDList.jsx
+++ src/components/UUIDList.jsx
@@ -1,7 +1,7 @@
 import React, { PropTypes } from 'react';
 import { FormInput } from 'elemental';
 
-const UUIDList = ({ uuids, count, version, delimiter, ...props }) => (
+const UUIDList = ({ uuids, delimiter, ...props }) => (
 	<div className="uuid-list">
 		<FormInput
 			style={{ height: 250 }}
@@ -13,15 +13,11 @@ const UUIDList = ({ uuids, count, version, delimiter, ...props }) => (
 
 UUIDList.propTypes = {
 	uuids: PropTypes.arrayOf(PropTypes.string),
-	count: PropTypes.number,
-	version: PropTypes.oneOf(['v1', 'v4']),
 	delimiter: PropTypes.string,
 };
 
 UUIDList.defaultProps = {
 	uuids: [],
-	count: 10,
-	version: 'v4',
 	delimiter: '\n',
 };
 
